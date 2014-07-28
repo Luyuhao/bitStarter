f07b7f0953eb081d18a7e565fedc9ff46f373811
Luyuhao <leopold.lu07@gmail.com>
2014-07-28 15:09:41 +0800

    HW1_read from index.html

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var app = express();
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-var indexBuffer = fs.readFileSync('/index.html');
+var fs = require('fs');
+var indexBuffer = fs.readFileSync('index.html');
 app.get('/', function(request, response) {
   response.send(indexBuffer.toString());
 })
@@ -12,3 +13,4 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
+
